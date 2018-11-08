ea500cf867e1d4c39fe38c74346f05ad174b34c0
Luis Fernando Florian <lflorian@mega.corp>
2018-11-08 10:32:35 -0600

    Refactorización de codigo

--- FirmaDocumento.cs
+++ FirmaDocumento.cs
@@ -1,38 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using FirmaXadesNet;
-using FirmaXadesNet.Clients;
 using FirmaXadesNet.Crypto;
 using FirmaXadesNet.Signature;
 using FirmaXadesNet.Signature.Parameters;
-using FirmaXadesNet.Upgraders;
-using FirmaXadesNet.Upgraders.Parameters;
-using FirmaXadesNet.Utils;
-using System.Security.Cryptography.Xml;
-using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml;
-using Microsoft.Xades;
-using System.Xml.Linq;
 
 namespace Firma
 {
     public class FirmaDocumento
     {
-
-        XadesService servicioXades = new XadesService();
-
+        //Invocación de la firma de documento, retorno  y almacenamiento de este
         public XmlDocument FirmarDocumento(string rutaCertificado, string contraseñaCertificado, string rutaDocumento, string ubicacionDestino)
         {
             X509Certificate2 cert = new X509Certificate2(rutaCertificado, contraseñaCertificado, X509KeyStorageFlags.Exportable | X509KeyStorageFlags.PersistKeySet);
-            XadesService xadesService = new XadesService();
 
             SignatureParameters parametros = ParametrosdeFirma();
-
             var nombredocumento = Path.GetFileNameWithoutExtension(rutaDocumento);
 
             using (parametros.Signer = new Signer(cert))
@@ -43,18 +26,19 @@ namespace Firma
             }
         }
 
+        //Invocación de la firma de documento y retorno de este
         public XmlDocument FirmarDocumento(string rutaCertificado, string contraseñaCertificado, string rutaDocumento)
         {
             X509Certificate2 cert = new X509Certificate2(rutaCertificado, contraseñaCertificado, X509KeyStorageFlags.Exportable | X509KeyStorageFlags.PersistKeySet);
 
             SignatureParameters parametros = ParametrosdeFirma();
-            
             using (parametros.Signer = new Signer(cert))
             {
                 return FirmaXades(parametros, rutaDocumento).Document;
             }
         }
 
+        //Firma del documento
         private SignatureDocument FirmaXades(SignatureParameters sp, string ruta)
         {
             XadesService xadesService = new XadesService();
@@ -66,12 +50,14 @@ namespace Firma
             }
         }
 
+        //Almacenamiento e ruta especifica
         private void AlmacenamientoDocumento(SignatureDocument sd, string ruta, string nombre)
         {
             ruta = $@"{ruta}\{nombre}-Firmado.xml";
             sd.Save(ruta);
         }
         
+        //Parametros para la firma del documento
         public SignatureParameters ParametrosdeFirma()
         {
             SignatureParameters parametros = new SignatureParameters
@@ -86,13 +72,12 @@ namespace Firma
             return parametros;
         }
         
+        //Cambio de posicion del nodo de la firma en el nodo padre del documento
         private void MoverNodoFirma(SignatureDocument sd)
         {
             var documento = sd.Document;
-            
             var NodoFirma = documento.GetElementsByTagName("ds:Signature")[0];
             NodoFirma.ParentNode.RemoveChild(NodoFirma);
-
             documento.DocumentElement.AppendChild(NodoFirma);
         }
     }
